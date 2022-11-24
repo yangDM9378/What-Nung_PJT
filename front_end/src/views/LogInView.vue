3d6f9ba938667e975be3c3262dd14fc0884377b3
yangDM9378 <coco1622555@gmail.com>
2022-11-24 21:38:00 +0900

    test

--- front_end/src/views/LogInView.vue
+++ front_end/src/views/LogInView.vue
@@ -81,7 +81,6 @@ a{
     width:100%;
     height: 100vh;
     background-color:#f5f1ee;
-    background-image:;
     background-position: center center;
     background-repeat: no-repeat;
     background-size:cover;
